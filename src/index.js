e44e0e65662b9da9a5f67043a3cbc2ebe6328147
Richard B <C:\Users\dmitr\AppData\Roaming\The Bat!>
2018-09-23 16:48:38 +0300

    try1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];// your implementation
+    this.comparator;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);// youеr implementation
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];// your implementation
   }
 
   get length() {
-    // your implementation
+    return this.array.length;// your implementation
   }
 
   toArray() {
-    // your implementation
+    return this.array.slice();// your implementation
   }
 
   sort(indices) {
-    // your implementation
+    let tempArray = [];// your implementation
+    indices.sort(( a, b ) =>  a - b);
+
+    for(let i = 0; i < indices.length; i++) {
+      tempArray.push(this.array[indices[i]]);
+    }
+
+    if(typeof this.comparator == 'undefined') {
+      tempArray.sort(( a, b ) =>  a - b);
+    } else {
+      tempArray.sort(this.comparator);
+    }
+    
+    for(let i = 0; i < tempArray.length; i++) {
+      this.array[indices[i]] = tempArray[i];
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;// your implementation
   }
 }
 
